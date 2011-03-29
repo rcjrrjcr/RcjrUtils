6d3cd521f9f6e86c9915ca6ee1d7c8ba869727c2
rcjrrjcr <ryanchewjingrong@yahoo.com.sg>
2011-03-29 21:52:27 +0800

    Completed SearchHelper.

--- src/com/rcjrrjcr/bukkitplugins/util/SearchHelper.java
+++ src/com/rcjrrjcr/bukkitplugins/util/SearchHelper.java
@@ -2,30 +2,54 @@ package com.rcjrrjcr.bukkitplugins.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Pattern;
-
+import java.util.Map;
+import java.util.Map.Entry;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+/**
+ * Utility class managing searching-related functions
+ * @author rcjrrjcr
+ */
 public class SearchHelper {
 
 	private HashSet<String> knownWords;
-	private static final Pattern firstWord= Pattern.compile("^(\\S*)\\s");
+//	private static final Pattern splitWords= Pattern.compile("\\b(\\w+)\\b");
 	public SearchHelper()
 	{
 		knownWords = new HashSet<String>();
 	}
-	
-	public void addWord(String word)
+	/**
+	 * Adds word to list of known words. If input contains spaces, it will be split into many words,
+	 * <br/> each word being delimited by a word boundary, will be added.
+	 * @param word New word
+	 */
+	public void addWord(String input)
 	{
-		String[] words = firstWord.split(" ");
-		for(String w : words)
-		{
-			knownWords.add(w);
-		}
+//		Matcher m = splitWords.matcher(input);
+//		while(m.find())
+//		{
+//			knownWords.add(m.group());
+//		}
+		if(input!=null)knownWords.add(input);
 	}
 	
-	public List<String> getMatches(String word)
+	public List<String> getMisses(String word)
+	{
+		 return getMisses(word,0);
+	}
+	/**
+	 * Get possible correct spellings of a misspelled word.
+	 * @param word Misspelled word
+	 * @param threshold Maximum Damerau-Levenshtein distance
+	 * @return List of possible near misses
+	 */
+	public List<String> getMisses(String word, final int threshold)
 	{
 		 if(knownWords.contains(word))
 		 {
@@ -33,22 +57,57 @@ public class SearchHelper {
 			 words.add(word);
 			 return words;
 		 }
-		 List<WeightedString> weightedWords = new ArrayList<WeightedString>(knownWords.size());
+		 Map<String,Integer> weightedWords = new HashMap<String,Integer>(knownWords.size());
 		 for(String s : knownWords)
 		 {
-			 weightedWords.add(new WeightedString(getDistance(word,s),s));
+			 int dist = getDistance(word,s);
+			 if(threshold <= 0||!(dist < threshold)) weightedWords.put(s,dist);
 		 }
-		 Collections.sort(weightedWords);
-		 List<String> wordList = new LinkedList<String>();
-		 for(WeightedString string : weightedWords)
+		 LinkedHashMap<String,Integer> sortedWords = sortMapByValue(weightedWords, new Comparator<Integer>()
 		 {
-			 wordList.add(string.getString());
-		 }
-		 return wordList;
+
+			@Override
+			public int compare(Integer arg0, Integer arg1) {
+				return arg0.compareTo(arg1);
+			}
+			 
+		 });
+		 return new ArrayList<String>(sortedWords.keySet());
 	}
-	
-	
-	
+	/**
+	 * Searches and returns for words in knownWords which contain the input word.
+	 * @param word Input word
+	 * @param threshold Maximum difference between the length of the input word and the current word to be checked.
+	 * @return List of words that contain the input word and are not longer than the length of the input word + <i>threshold</i>.
+	 */
+	public List<String> search(String word, final int threshold)
+	{
+//		List<String> nearMisses = getMisses(word, threshold);
+		List<String> results = new LinkedList<String>();
+		for(String known : knownWords)
+		{
+			if(known.length() < word.length()) continue;
+			if(threshold > 0 && known.length() - word.length() > threshold) continue;
+			if(known.contains(word)) results.add(word);
+		}
+		Collections.sort(results, new Comparator<String>(){
+
+			@Override
+			public int compare(String arg0, String arg1) {
+				return new Integer(arg0.length()).compareTo(new Integer(arg1.length()));
+			}
+			
+		});
+//		results.addAll(nearMisses);
+		return results;
+		
+	}
+	/**
+	 * Calculates Damerau-Levenshtein distance
+	 * @param word First word
+	 * @param ref Second word
+	 * @return Damerau-Levenshtein distance
+	 */
 	private Integer getDistance(String word, String ref)
 	{
 		//TODO: Optimi[zs]e code to find the Damerau-Levenshtein distance
@@ -79,12 +138,21 @@ public class SearchHelper {
 				}
 			}			
 		}
-		
 		return mat[wordLength][refLength];
 	}
 	
+	public void clear()
+	{
+		knownWords.clear();
+	}
+	/**
+	 * Utility function to get the minimum of all parameters.
+	 * @param values Integers to compare
+	 * @return Least integer
+	 */
 	private final static int minimum(int... values)
 	{
+		if(values.length==0) return 0;
 		int min = values[0];
 		for(int val : values)
 		{
@@ -96,30 +164,49 @@ public class SearchHelper {
 		
 		return min;
 	}
+	
+	
+//	private final static <E extends Comparable<E>> E minimum( E... values)
+//	{
+//		if(values.length==0) return null;
+//		E min = values[0];
+//		for(E val : values)
+//		{
+//			if(min.compareTo(val)==1)
+//			{
+//				min = val;
+//			}
+//		}
+//		
+//		return min;
+//	}
+	/**
+	 * Utility function to sort maps by their values.
+	 * @param <K> Class of key
+	 * @param <V> Class of values
+	 * @param unsortedMap Map to be sorted
+	 * @param comparator Comparator to use on the values
+	 * @return A LinkedHashMap, sorted by values. 
+	 */
+	public final static <K,V> LinkedHashMap<K,V> sortMapByValue(Map<K,V> unsortedMap, final Comparator<V> comparator)
+	{
+		if(unsortedMap.size() <= 1) return new LinkedHashMap<K,V>(unsortedMap);
+		List<Map.Entry<K, V>> entryList = new ArrayList<Map.Entry<K, V>>(unsortedMap.entrySet());
+		Collections.sort(entryList, new Comparator<Map.Entry<K, V>>()
+		{
+			@Override
+			public int compare(Entry<K, V> o1, Entry<K, V> o2) {
+				return comparator.compare(o1.getValue(), o2.getValue());
+			}
+		}
+		);
+		LinkedHashMap<K,V> sortedMap = new LinkedHashMap<K, V>(unsortedMap.size());
+		for(Map.Entry<K, V> entry : entryList)
+		{
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		return sortedMap;
+	}
 }
 
-class WeightedString implements Comparable<WeightedString>
-{
-	private int val;
-	private String string;
-	public int getVal() {
-		return val;
-	}
-	public void setVal(int val) {
-		this.val = val;
-	}
-	public String getString() {
-		return string;
-	}
-	public void setString(String string) {
-		this.string = string;
-	}
-	public WeightedString(int val, String string) {
-		this.val = val;
-		this.string = string;
-	}
-	@Override
-	public int compareTo(WeightedString o) {
-		return new Integer(this.val).compareTo(new Integer(o.val));
-	}
-}
+
